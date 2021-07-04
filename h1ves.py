6c28584550c9c40722762a0087cd840fefefed00
mariusz <mariusz@mud.tk>
2021-07-04 22:33:40 +0200

    Trying to access environment variables on Heroku.

--- h1ves.py
+++ h1ves.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from boto.s3.connection import S3Connection
 
 app = Flask(__name__)
 
@@ -6,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def start():
     return {'status': 'ok',
-            'H1VES_STAGING_API_KEY': env['H1VES_STAGING_API_KEY']}
+            'H1VES_STAGING_API_KEY': S3Connection(os.environ['H1VES_STAGING_API_KEY'])}
 
 
 if __name__ == '__main__':
