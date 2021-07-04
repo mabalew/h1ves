a0e5fd653610d16999c4f14a166893d94d9f0fff
mariusz <mariusz@mud.tk>
2021-07-04 22:14:18 +0200

    Testing access to environment variables on Heroku.

--- h1ves.py
+++ h1ves.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return {'status': 'ok'}
+    return {'status': 'ok',
+            'H1VES_STAGING_API_KEY': env['H1VES_STAGING_API_KEY']}
 
 if __name__ == '__main__':
     app.run()
