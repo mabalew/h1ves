f4dcce6988559bda0fc93748e5e6d806f9bfde53
mariusz <mariusz@mud.tk>
2021-07-04 22:44:59 +0200

    Trying to access environment variables on Heroku.

--- h1ves.py
+++ h1ves.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def start():
     return {'status': 'ok',
-            'H1VES_STAGING_API_KEY': os.environ('H1VES_STAGING_API_KEY')}
+            'H1VES_STAGING_API_KEY': os.environ['H1VES_STAGING_API_KEY']}
 
 if __name__ == '__main__':
     app.run()
